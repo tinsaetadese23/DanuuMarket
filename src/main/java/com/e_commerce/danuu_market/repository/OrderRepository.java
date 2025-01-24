53fc897b2a0a81e9b72b39ab7d6a93e10d2e759d
Tinsae Tadese <tinsaetadese23@gmail.com>
2025-01-24 21:48:15 +0300

    OrderRepository setup

--- src/main/java/com/e_commerce/danuu_market/repository/OrderRepository.java
+++ src/main/java/com/e_commerce/danuu_market/repository/OrderRepository.java
@@ -3,6 +3,11 @@ package com.e_commerce.danuu_market.repository;
 import com.e_commerce.danuu_market.models.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ServiceRepository extends JpaRepository<Orders, Integer> {
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Orders, Integer> {
+public Orders findById(int id);
+public List<Orders> findAll();
+
 
 }
