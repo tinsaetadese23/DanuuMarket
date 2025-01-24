6b5c300a7df8bd2b139d7a7ca99eb741c58aa729
Tinsae Tadese <tinsaetadese23@gmail.com>
2025-01-24 22:13:57 +0300

    findOrder setup

--- src/main/java/com/e_commerce/danuu_market/controllers/OrderController.java
+++ src/main/java/com/e_commerce/danuu_market/controllers/OrderController.java
@@ -7,11 +7,13 @@ import com.e_commerce.danuu_market.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -20,9 +22,10 @@ public class OrderController {
 
     @Autowired
     OrderService orderService;
-
     Map<String, String> order_responses = new HashMap<>();
-    public ResponseEntity<?> findAllOrders(@RequestParam int id){
+
+    @GetMapping("/getOrder")
+    public ResponseEntity<?> findOrder(@RequestParam int id){
         Orders orders = orderService.findOrder(id);
         if(orders == null){
             order_responses.put("error","No order found with this ID");
@@ -31,4 +34,17 @@ public class OrderController {
         }
         return ResponseEntity.ok(orders);
     }
+
+    @GetMapping("/get")
+    public ResponseEntity<?> findAllOrders(){
+        List<Orders> orders = orderService.findAllOrders();
+        if(orders.isEmpty()){
+            order_responses.put("error","There is no order");
+            order_responses.put("error_code","-1");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(order_responses);
+        }
+
+        return ResponseEntity.ok(orders);
+    }
+
 }
